df9373c168702b67eb81b92b9e2cc8683b9b80ad
13cyberpunk02 <salawat1302@gmail.com>
2026-09-24 17:17:11 +0300

    Мелкое исправление, убрал лишние файлы из списка angular.json и derecated provideAnimationsAsync()

--- src/Chameleon.UI/src/app/app.config.ts
+++ src/Chameleon.UI/src/app/app.config.ts
@@ -12,7 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient((withInterceptors([authInterceptor]))),
-    provideAnimationsAsync(),
     provideCharts(withDefaultRegisterables()),
   ]
 };
